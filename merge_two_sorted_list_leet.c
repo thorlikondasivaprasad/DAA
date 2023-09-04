ecd92ca348e8a7e09a4a2d3606c6b073470e0231
Sivaprasad0734 <108944251+thorlikondasivaprasad@users.noreply.github.com>
2023-09-04 09:00:08 +0530

    merge_two_sorted_list_leet

---
+++ merge_two_sorted_list_leet.c
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+ struct ListNode *mergeTwoLists(struct ListNode *list1, 
+                               struct ListNode *list2) {
+    struct ListNode *head;
+    struct ListNode *temp;
+
+    if (list1 == NULL || list2 == NULL)
+        return (list1) ? list1 : list2;
+    if (list1->val <= list2->val) {
+        head = temp = list1;
+        list1 = list1->next;
+    }
+    else {
+        head = temp = list2;
+        list2 = list2->next;
+    }
+    while (list1 && list2) {
+        if (list1->val <= list2->val) {
+            temp->next = list1;
+            list1 = list1->next;
+        }
+        else {
+            temp->next = list2;
+            list2 = list2->next;
+        }
+        temp = temp->next;
+    }
+    temp->next = (list1 == NULL) ? list2 : list1;
+    return (head);
+}
