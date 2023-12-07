76ec9f0e8bfc8ca1a25093261b86674b42bb7ca8
Sivaprasad0734 <108944251+thorlikondasivaprasad@users.noreply.github.com>
2023-12-07 05:56:28 +0530

    Fractional_knapsack.c

--- knapsack.c
+++ knapsack.c
@@ -1,49 +1,31 @@
-/* Online C Compiler and Editor */
-/* Online C Compiler and Editor */
-#include <stdio.h>
-void main()
-{
-    int capacity, no_items, cur_weight, item;
-    int used[10];
-    float total_profit;
-    int i;
-    int weight[10];
-    int value[10];
-    printf("Enter the capacity of knapsack:\n");
-    scanf("%d", &capacity);
-    printf("Enter the number of items:\n");
-    scanf("%d", &no_items);
-    printf("Enter the weight and value of %d item:\n", no_items);
-    for (i = 0; i < no_items; i++)
-    {
-        printf("Weight[%d]:\t", i);
-        scanf("%d", &weight[i]);
-        printf("Value[%d]:\t", i);
-        scanf("%d", &value[i]);
-    }
-    for (i = 0; i < no_items; ++i)
-        used[i] = 0;
-
-    cur_weight = capacity;
-    while (cur_weight > 0)
-    {
-        item = -1;
-        for (i = 0; i < no_items; ++i)
-            if ((used[i] == 0) && ((item == -1) || ((float) value[i] / weight[i] > (float)value[item]/ weight[item])))
-                item = i;
-                
-        used[item] = 1;
-        cur_weight -= weight[item];
-        if (cur_weight >= 0){
-            total_profit += value[item];
-            printf("Added object %d (%d Rs., %dKg) completely in the bag. Space left: %d.\n", item + 1, value[item], weight[item], cur_weight);
-        }
-        else
-        {
-            int item_percent = (int) ((1 + (float) cur_weight / weight[item]) * 100);
-            printf("Added %d%% (%d Rs., %dKg) of object %d in the bag.\n", item_percent, value[item], weight[item], item + 1);
-            total_profit = total_profit+(1 + (float)cur_weight / weight[item]) * value[item];
-        }
-    }
-    printf("Filled the bag with objects worth %.2f Rs.\n", total_profit);
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	float wt[7]={1,3,5,4,1,3,3};
+	float pf[7]={5,10,15,7,8,9,4};
+	int used[7];
+	int mw=15;
+	int cur_w=mw,i,maxi;
+	float profit=0;
+	for(int i=0;i<7;i++)
+	     used[i]=0;
+	while(cur_w>0){
+		maxi=-1;
+		for(int i=0;i<7;i++){
+			float pfwt=pf[i]/wt[i];
+			if(used[i]==0 &&(maxi==-1||pfwt>(pf[maxi]/wt[maxi]))){
+				maxi=i;
+			}
+	}
+	//printf("%d ",maxi);
+	 used[maxi]=1;
+	 cur_w-=wt[maxi];
+	 profit+=pf[maxi];
 }
+     profit+=(cur_w/wt[maxi])*pf[maxi];
+	printf("%.5f",profit);
+}
+
+Oitput:
+
+50.5000
