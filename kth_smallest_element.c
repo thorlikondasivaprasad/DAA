dd322cc834fb9dc68e864279ef34b4767a737237
Sivaprasad0734 <108944251+thorlikondasivaprasad@users.noreply.github.com>
2023-12-30 12:53:52 +0530

    kth_smallest_element.c

---
+++ kth_smallest_element.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void merge(int a[], int l, int mid, int r) {
+  int i, j, k;
+  int n1 = mid - l + 1;
+  int n2 = r - mid;
+
+  int L[n1], R[n2];
+
+  for (i = 0; i < n1; i++) {
+    L[i] = a[l + i];
+  }
+  for (j = 0; j < n2; j++) {
+    R[j] = a[mid + 1 + j];
+  }
+
+  i = 0, j = 0, k = l;
+
+  while (i < n1 && j < n2) {
+    if (L[i] <= R[j]) {
+      a[k] = L[i];
+      i++;
+    } else {
+      a[k] = R[j];
+      j++;
+    }
+    k++;
+  }
+  while (i < n1) {
+    a[k] = L[i];
+    i++;
+    k++;
+  }
+  while (j < n2) {
+    a[k] = R[j];
+    j++;
+    k++;
+  }
+}
+void mergesort(int a[], int l, int r) {
+  int mid;
+  if (l < r) {
+    mid = l + (r - l) / 2;
+    mergesort(a, l, mid);
+    mergesort(a, mid + 1, r);
+    merge(a, l, mid, r);
+  }
+}
+int printarray(int a[], int size) {
+  printf("The elements of the array : ");
+  for (int i = 0; i < size; i++) {
+    printf("%d ", a[i]);
+  }
+}
+int main() {
+  // int a[]={13,12,11,43,3,14};
+  int i, n;
+  printf("Enter no.of elements in array :");
+  scanf("%d", &n);
+  int a[n];
+  for (i = 0; i < n; i++) {
+    scanf("%d", &a[i]);
+  }
+  int size = sizeof(a) / sizeof(a[0]);
+  printarray(a, size);
+  mergesort(a, 0, size - 1);
+  printf("\n Resultant Array :\n");
+  printarray(a, size);
+  int s; // for kth smallest element
+  printf("\n Enter the index to delete the kth smallest :");
+  scanf("%d", &s);
+  printf("%d", a[s - 1]);
+  return 0;
+}
