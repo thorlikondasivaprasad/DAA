720b66087d8dd898c73381f9c167b1a12e57ff46
Sivaprasad0734 <108944251+thorlikondasivaprasad@users.noreply.github.com>
2023-12-30 16:11:11 +0530

    Update Dijkstras_Greedy_.c

--- Dijkstras_Greedy_.c
+++ Dijkstras_Greedy_.c
@@ -1,68 +1,64 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #define inf 9999
-int mindist(int dist[],int visited[]){
-	int min=inf,mi=0;
-	for(int i=0;i<9;i++){
-		if(dist[i]<min&&!(visited[i])){
-			min=dist[i];
-			mi=i;
-		}
-	}
-	return mi;
+
+int mindist(int dist[],int visited[],int n)
+{
+  int mi=0;
+  int min=inf;
+  for(int i=0;i<n;i++)
+    {
+       if(dist[i] < min && !visited[i])
+       {
+         min = dist[i];
+         mi=i;
+       }
+    }
+  return mi;
 }
+
 int main()
 {
-	int graph[10][10]={{0,4,inf,inf,inf,inf,inf,8,inf},
-						{4,0,8,inf,inf,inf,inf,11,inf},
-						{inf,8,0,7,inf,4,inf,inf,2},
-						{inf,inf,7,0,9,14,inf,inf,inf},
-						{inf,inf,inf,9,0,10,inf,inf,inf},
-						{inf,inf,4,inf,10,0,2,inf,inf},
-						{inf,inf,inf,inf,inf,2,0,1,6},
-						{8,11,inf,inf,inf,inf,1,0,7},
-						{inf,inf,2,inf,inf,inf,6,7,0}};
-	int visited[9],dist[9],no_edge;
-	memset(visited,0,9*sizeof(int));
-	memset(dist,inf,9*sizeof(int));
-	visited[0]=1;
-	dist[0] = 0;
-	while(no_edge<8){
-		int u=mindist(dist,visited);
-		//printf("\n %d  %d ",u,dist[u]);
-		visited[u]=1;
-		for (int i = 0; i < 9; i++)
-		{
-             if (dist[u]+graph[u][i]<dist[i] && !visited[i])
-                 dist[i]=dist[u]+graph[u][i];
-		}
-        no_edge++;
+  int n;
+  printf("Enter no.of vertices : ");
+  scanf("%d",&n);
+  int graph[n][n];
+  printf("Enter the adj matrix :  \n");
+  for (int i=0;i<n;i++)
+    {
+      for(int j=0;j<n;j++)
+        {
+          scanf("%d",&graph[i][j]);
+          if (graph[i][j]==-1)
+            graph[i][j]=inf;
+        }
     }
-	printf("Vertex Cost\n");
-	for(int i=0;i<9;i++)
-	printf("%d %d\n",i+1,dist[i]);
-	}
+  int dist[n],visited[n],edges=0;
+  memset(visited, 0, n * sizeof(int));
+  memset(dist, inf, n * sizeof(int));
 
-Output:
+  dist[0]=0;
+  visited[0]=1;
 
-Vertex Cost
-
-1 0
-
-2 4
-
-3 12
-
-4 19
-
-5 21
-
-6 11
-
-7 9
-
-8 8
-
-9 14
+  while(edges < n-1)
+    {
+      int min = mindist(dist,visited,n);
+      visited[min]=1;
+      for(int i=0;i<n;i++)
+        {
+          if(dist[min] + graph[min][i] < dist[i] && !visited[i])
+          {
+            dist[i] = dist[min] + graph[min][i];
+          }
+        }
+      edges++;
+    }
+  printf("Vertex cost is: ");
+  for(int i=0;i<n;i++)
+    {
+      printf("%d  %d \n",i+1,dist[i]);
+    }
+  return 0;
+}
 
