b9203d7923c3dae515bd88eb60cc1a82dba99dce
Sivaprasad0734 <108944251+thorlikondasivaprasad@users.noreply.github.com>
2024-01-09 22:06:27 +0530

    Update Coin_change_DP.c

--- Coin_change_DP.c
+++ Coin_change_DP.c
@@ -1,45 +1,45 @@
-#include<stdio.h>
-#include<stdlib.h>
-#define INT_MAX 999999
-int min(int a,int b){
-	if(a<b)
-	   return a;
-	else
-	   return b;
+#include <stdio.h>
+#include <limits.h>
+
+int minCoins(int coins[], int n, int sum)
+{
+    int dp[sum + 1];
+    int usedCoins[sum + 1][n];
+    dp[0] = 0;
+
+    for (int i = 1; i <= sum; i++)
+        dp[i] = INT_MAX;
+
+    for (int i = 0; i <= sum; i++) {
+        for (int j = 0; j < n; j++) {
+            if (coins[j] <= i) {
+                int sub_res = dp[i - coins[j]];
+                if (sub_res != INT_MAX && sub_res + 1 < dp[i]) {
+                    dp[i] = sub_res + 1;
+                    for (int k = 0; k < n; k++) {
+                        usedCoins[i][k] = usedCoins[i - coins[j]][k];
+                    }
+                    usedCoins[i][j]++;
+                }
+            }
+        }
+    }
+
+    printf("Coins used: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", usedCoins[sum][i]);
+    }
+    printf("\n");
+
+    return dp[sum];
 }
-int coinchange(int ind,int tgt,int coins[] ){
-	if(ind==0){
-		if(tgt%coins[0]==0)
-		    return tgt/coins[0];
-		else
-		    return -1;
-	}
-	int nottake=coinchange(ind-1,tgt,coins);
-	int take=INT_MAX;
-	if(coins[ind]<=tgt){
-		take=1+coinchange(ind,tgt-coins[ind],coins);	
-	}
-	return min(take,nottake);
+
+int main()
+{
+    int coins[] = { 1, 2, 3 };
+    int n = sizeof(coins) / sizeof(coins[0]);
+    int sum = 5;
+
+    printf("Minimum number of coins: %d\n", minCoins(coins, n, sum));
+    return 0;
 }
-int main(){
-	int n,tgt,res;
-	printf("\n Enter no.of different denominations:");
-	scanf("%d",&n);
-	int coins[10];
-	for(int i=0;i<n;i++){
-		scanf("%d",&coins[i]);
-	}
-	printf("\n Enter the target:");
-	scanf("%d",&tgt);
-	res=coinchange(n-1,tgt,coins);
-	printf("%d is solution..",res);
-}
-
-
-
-Output:
-
-Enter no.of different denominations:4
-1 5 6 9
-Enter the target:11
-2 is solution 
