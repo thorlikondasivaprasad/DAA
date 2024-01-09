0ffd0c812412499d6087697b5cea82811abd6e43
Sivaprasad0734 <108944251+thorlikondasivaprasad@users.noreply.github.com>
2024-01-09 21:55:34 +0530

    Update N_queens_Backtracking.c

--- N_queens_Backtracking.c
+++ N_queens_Backtracking.c
@@ -1,45 +1,45 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <limits.h>
 
-int x[10];
+int minCoins(int coins[], int n, int sum)
+{
+    int dp[sum + 1];
+    int usedCoins[sum + 1][n];
+    dp[0] = 0;
 
-int place(int queeno, int i) {
-	int j;
-    for(j = 0; j < queeno; j++) {
-        if(x[j] == i || abs(x[j] - i) == abs(j - queeno)) {
-            return 0;
-        }
-    }
-    return 1;
-}
-void nqueen(int queeno, int n) {
-	int i,j;
-    for(i = 0; i < n; i++) {
-        if(place(queeno, i+1)) {
-            x[queeno] = i+1;
-            if(queeno == n-1) {
-                for(j = 0; j < n; j++) {
-                    printf("%d ", x[j]);
+    for (int i = 1; i <= sum; i++)
+        dp[i] = INT_MAX;
+
+    for (int i = 0; i <= sum; i++) {
+        for (int j = 0; j < n; j++) {
+            if (coins[j] <= i) {
+                int sub_res = dp[i - coins[j]];
+                if (sub_res != INT_MAX && sub_res + 1 < dp[i]) {
+                    dp[i] = sub_res + 1;
+                    for (int k = 0; k < n; k++) {
+                        usedCoins[i][k] = usedCoins[i - coins[j]][k];
+                    }
+                    usedCoins[i][j]++;
                 }
-                printf("\n");
-            } else {
-                nqueen(queeno + 1, n);
             }
         }
     }
+
+    printf("Coins used: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", usedCoins[sum][i]);
+    }
+    printf("\n");
+
+    return dp[sum];
 }
 
-int main() {
-    int n, i = 0;
-    printf("Enter the number of queens: ");
-    scanf("%d", &n);
-    nqueen(i, n);
+int main()
+{
+    int coins[] = { 1, 2, 3 };
+    int n = sizeof(coins) / sizeof(coins[0]);
+    int sum = 5;
+
+    printf("Minimum number of coins: %d\n", minCoins(coins, n, sum));
     return 0;
 }
-
-
-Output:
-
-Enter the number of queens: 4
-2 4 1 3 
-3 1 4 2 
